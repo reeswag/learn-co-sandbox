423909d99ed4e3d4a7a0b90fcf76311114bdae27
William Rees <reeswag@gmail.com>
2018-09-09 11:54:35 +0000

    automatically backed up by learn

---
+++ VariableAssignment.js
@@ -0,0 +1,19 @@
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+
+var a = 5
+var b = 2
+var c = 3
+var d = 'hello'
+var e = 'goodbye'
+console.log(a)
